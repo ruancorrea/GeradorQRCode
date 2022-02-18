5a6748721e99995d32c3403b0060a0c92db97811
ruancorrea <rhcs@ic.ufal.br>
2022-02-18 15:24:53 -0300

    hscreen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,13 @@ import QRcode from "../components/QRcode";
 
 export default function Home() {
   return (
-   <div className={`bg-gradient-to-r from-gray-100 to-gray-200`}>
+   <div className={`h-screen bg-gradient-to-r from-gray-100 to-gray-200`}>
      
-     <header className="p-6 bg-slate-700 mb-2">
-     <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <div className="flex w-screen items-center flex-shrink-0 text-white mr-6 p-6 bg-slate-700 mb-2">
         {IconQRcode}
         <span className="font-semibold text-xl tracking-tight ml-2">Gerador de QR Code</span>
       </div>
-     </header>
-     <div className="flex w-screen h-screen justify-center">
+     <div className="flex w-screen justify-center">
       <QRcode />
 
      </div>
